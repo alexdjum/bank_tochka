5236cd9412f2cab9104c42a115d55d641c57b4f8
alexdjum <killer260598@mail.ru>
2025-05-11 19:48:58 +0300

    add run2.py

---
+++ run2.py
@@ -0,0 +1,27 @@
+import sys
+
+import collections
+
+
+# Константы для символов ключей и дверей
+keys_char = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+doors_char = [k.upper() for k in keys_char]
+
+
+def get_input():
+    """Чтение данных из стандартного ввода."""
+    return [list(line.strip()) for line in sys.stdin]
+
+
+def solve(data):
+    ...
+
+
+def main():
+    data = get_input()
+    result = solve(data)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
